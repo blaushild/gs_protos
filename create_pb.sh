cf39ecc73f1be34b4c61c228c6d6698f45ec40ac
blaushild <3betpush@gmail.com>
2025-03-20 09:19:16 +0300

    Added auth service

--- create_pb.sh
+++ create_pb.sh
@@ -5,3 +5,6 @@ protoc -I proto proto/geoservice/geoservice.proto --go_out=./gen/go --go_opt=pat
 
 # user
 protoc -I proto proto/userpb/user.proto --go_out=./gen/go --go_opt=paths=source_relative --go-grpc_out=./gen/go/ --go-grpc_opt=paths=source_relative
+
+# auth
+protoc -I proto proto/authpb/auth.proto --go_out=./gen/go --go_opt=paths=source_relative --go-grpc_out=./gen/go/ --go-grpc_opt=paths=source_relative
